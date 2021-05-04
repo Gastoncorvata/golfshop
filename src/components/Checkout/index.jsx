55a0ee6f8a8af5b48c56df1ed1e54668c0df7721
Gaston Corvalan <corvata@gmail.com>
2021-05-03 23:47:54 -0300

    Entrega Final

--- src/components/Checkout/index.jsx
+++ src/components/Checkout/index.jsx
@@ -4,13 +4,16 @@ import firebase from "firebase/app";
 import { getFirestore } from "../../firebase";
 import "firebase/firestore";
 
+const historyGoBack = () => {
+	window.history.go(-3);
+};
 
 const Checkout = () => {
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
 	const [phone, setPhone] = useState("");
 	const [adress, setAdress] = useState("");
-	const { cart, totalPrecio } = useContext(CartContext);
+	const { cart, totalPrecio, clear } = useContext(CartContext);
 	const [idOrden, setIdOrden] = useState(null);
 
 
@@ -23,6 +26,10 @@ const guardarOrden = (e) => {
 	const ordersCollection = db.collection("orders");
 
 	const date = firebase.firestore.Timestamp.fromDate(new Date());
+
+	setTimeout(() => {
+		clear();
+	}, 1000);
 
 	const items = cart.map((cartItem) => {
 		return {
@@ -70,6 +77,12 @@ const guardarOrden = (e) => {
 				<p className="bg-emerald-200 font-medium p-8 rounded-full shadow-2xl text-3xl text-white w-10/12">
 					{idOrden ? `Your order was made successfully. YOUR ORDER IS: ${idOrden}` : null}
 				</p>
+				<button
+					onClick={historyGoBack}
+					className="bg-teal-500 border-b-4 border-green-700 font-bold hover:bg-blueGray-400 m-4 px-4 py-2 rounded text-white"
+				>
+					Go Back
+				</button>
 			</div>
 			<h3 className="font-bold text-3xl text-blue-400 text-center">
 				Fill in your details to confirm the purchase!{" "}
